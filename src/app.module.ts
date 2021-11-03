fe1230d98580056d3727d51c2ef1b78e9c03e42c
jinxin_fl <972784646@qq.com>
2021-11-03 12:27:15 +0800

    feat: 区分生产和开发环境的mysql密码

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ import { PvModule } from './monitor/pv/pv.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'q847164495',
+      password: process.env.NODE_ENV === 'production' ? 'q847164495' : '123',
       database: 'nest_admin',
       synchronize: true,
       autoLoadEntities: true,
