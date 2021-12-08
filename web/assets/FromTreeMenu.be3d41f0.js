2f3dcf89d0af0b218b029ac872ece535ceeb54cf
jinxin_fl <972784646@qq.com>
2021-12-08 16:31:44 +0800

    packge: 前端打包

---
+++ web/assets/FromTreeMenu.be3d41f0.js
@@ -0,0 +1,1 @@
+import{h as a}from"./index.620972d0.js";const m=async m=>a("/api/menu",{method:"POST",data:m}),n=async m=>a(`/api/menu/${m}`,{method:"DELETE"}),s=async(m,n)=>a(`/api/menu/${m}`,{method:"PATCH",data:n}),d=async m=>a("/api/menu/many",{method:"POST",data:m});export{d as a,m as c,n as d,s as u};
