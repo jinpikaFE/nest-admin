a2db449515989160179434ca608d435ba71a3558
jinxin <jinxin@hsmap.com>
2023-06-02 11:19:36 +0800

    feat: 日志改elk

--- src/config/log4js.ts
+++ src/config/log4js.ts
@@ -1,67 +1,48 @@
 import * as path from 'path';
-const baseLogPath = path.resolve(__dirname, '../../logs'); // 日志要写入哪个目录
 
 const log4jsConfig = {
   appenders: {
+    /** logstash 配置 */
+    logstash: {
+      type: '@log4js-node/logstash-http',
+      url: 'http://192.168.56.10:9200/_bulk',
+      application: 'nest-admin',
+      logType: 'application',
+      logChannel: 'node',
+    },
     console: {
       type: 'console', // 会打印到控制台
     },
     access: {
-      type: 'dateFile', // 会写入文件，并按照日期分类
-      filename: `${baseLogPath}/access/access.log`, // 日志文件名，会命名为：access.20200320.log
-      alwaysIncludePattern: true,
-      pattern: 'yyyyMMdd',
-      daysToKeep: 60,
-      numBackups: 3,
-      category: 'http',
-      keepFileExt: true, // 是否保留文件后缀
+      type: '@log4js-node/logstash-http',
+      url: 'http://192.168.56.10:9200/_bulk',
+      application: 'nest-admin-access',
+      logType: 'access',
+      logChannel: 'node',
     },
     app: {
-      type: 'dateFile',
-      filename: `${baseLogPath}/app-out/app.log`,
-      alwaysIncludePattern: true,
-      layout: {
-        type: 'pattern',
-        pattern:
-          '{"date":"%d","level":"%p","category":"%c","host":"%h","pid":"%z","data":\'%m\'}',
-      },
-      // 日志文件按日期（天）切割
-      pattern: 'yyyyMMdd',
-      daysToKeep: 60,
-      // maxLogSize: 10485760,
-      numBackups: 3,
-      keepFileExt: true,
-    },
-    errorFile: {
-      type: 'dateFile',
-      filename: `${baseLogPath}/errors/error.log`,
-      alwaysIncludePattern: true,
-      layout: {
-        type: 'pattern',
-        pattern:
-          '{"date":"%d","level":"%p","category":"%c","host":"%h","pid":"%z","data":\'%m\'}',
-      },
-      // 日志文件按日期（天）切割
-      pattern: 'yyyyMMdd',
-      daysToKeep: 60,
-      // maxLogSize: 10485760,
-      numBackups: 3,
-      keepFileExt: true,
+      type: '@log4js-node/logstash-http',
+      url: 'http://192.168.56.10:9200/_bulk',
+      application: 'nest-admin-app',
+      logType: 'app',
+      logChannel: 'node',
     },
     errors: {
-      type: 'logLevelFilter',
-      level: 'ERROR',
-      appender: 'errorFile',
+      type: '@log4js-node/logstash-http',
+      url: 'http://192.168.56.10:9200/_bulk',
+      application: 'nest-admin',
+      logType: 'errors',
+      logChannel: 'node',
     },
   },
   categories: {
     default: {
-      appenders: ['app', 'errors'],
+      appenders: ['app', 'errors', 'logstash'],
       level: 'DEBUG',
     },
-    info: { appenders: ['app', 'errors'], level: 'info' },
-    access: { appenders: ['app', 'errors'], level: 'info' },
-    http: { appenders: ['access'], level: 'DEBUG' },
+    info: { appenders: ['app', 'errors', 'logstash'], level: 'info' },
+    access: { appenders: ['app', 'errors', 'logstash'], level: 'info' },
+    http: { appenders: ['access', 'logstash'], level: 'DEBUG' },
   },
   pm2: true, // 使用 pm2 来管理项目时，打开
   pm2InstanceVar: 'INSTANCE_ID', // 会根据 pm2 分配的 id 进行区分，以免各进程在写日志时造成冲突
