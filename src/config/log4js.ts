f51c69c68a274085fca283fb7b8b94ce8d31f57b
jinxin <847164495@qq.com>
2024-03-07 15:44:13 +0800

    feat: 日志系统重新改为文件日志

--- src/config/log4js.ts
+++ src/config/log4js.ts
@@ -1,48 +1,120 @@
+// import * as path from 'path';
+
+// const log4jsConfig = {
+//   appenders: {
+//     /** logstash 配置 */
+//     logstash: {
+//       type: '@log4js-node/logstash-http',
+//       url: 'http://110.40.192.199:9200/_bulk',
+//       application: 'nest-admin',
+//       logType: 'application',
+//       logChannel: 'node',
+//     },
+//     console: {
+//       type: 'console', // 会打印到控制台
+//     },
+//     access: {
+//       type: '@log4js-node/logstash-http',
+//       url: 'http://110.40.192.199:9200/_bulk',
+//       application: 'nest-admin-access',
+//       logType: 'access',
+//       logChannel: 'node',
+//     },
+//     app: {
+//       type: '@log4js-node/logstash-http',
+//       url: 'http://110.40.192.199:9200/_bulk',
+//       application: 'nest-admin-app',
+//       logType: 'app',
+//       logChannel: 'node',
+//     },
+//     errors: {
+//       type: '@log4js-node/logstash-http',
+//       url: 'http://110.40.192.199:9200/_bulk',
+//       application: 'nest-admin',
+//       logType: 'errors',
+//       logChannel: 'node',
+//     },
+//   },
+//   categories: {
+//     default: {
+//       appenders: ['app', 'errors', 'logstash'],
+//       level: 'DEBUG',
+//     },
+//     info: { appenders: ['app', 'errors', 'logstash'], level: 'info' },
+//     access: { appenders: ['app', 'errors', 'logstash'], level: 'info' },
+//     http: { appenders: ['access', 'logstash'], level: 'DEBUG' },
+//   },
+//   pm2: true, // 使用 pm2 来管理项目时，打开
+//   pm2InstanceVar: 'INSTANCE_ID', // 会根据 pm2 分配的 id 进行区分，以免各进程在写日志时造成冲突
+// };
+
+// export default log4jsConfig;
+// config/log4js.ts
+
 import * as path from 'path';
+const baseLogPath = path.resolve(__dirname, '../../logs'); // 日志要写入哪个目录
 
 const log4jsConfig = {
   appenders: {
-    /** logstash 配置 */
-    logstash: {
-      type: '@log4js-node/logstash-http',
-      url: 'http://110.40.192.199:9200/_bulk',
-      application: 'nest-admin',
-      logType: 'application',
-      logChannel: 'node',
-    },
     console: {
       type: 'console', // 会打印到控制台
     },
     access: {
-      type: '@log4js-node/logstash-http',
-      url: 'http://110.40.192.199:9200/_bulk',
-      application: 'nest-admin-access',
-      logType: 'access',
-      logChannel: 'node',
+      type: 'dateFile', // 会写入文件，并按照日期分类
+      filename: `${baseLogPath}/access/access.log`, // 日志文件名，会命名为：access.20200320.log
+      alwaysIncludePattern: true,
+      pattern: 'yyyyMMdd',
+      daysToKeep: 60,
+      numBackups: 3,
+      category: 'http',
+      keepFileExt: true, // 是否保留文件后缀
     },
     app: {
-      type: '@log4js-node/logstash-http',
-      url: 'http://110.40.192.199:9200/_bulk',
-      application: 'nest-admin-app',
-      logType: 'app',
-      logChannel: 'node',
+      type: 'dateFile',
+      filename: `${baseLogPath}/app-out/app.log`,
+      alwaysIncludePattern: true,
+      layout: {
+        type: 'pattern',
+        pattern:
+          '{"date":"%d","level":"%p","category":"%c","host":"%h","pid":"%z","data":\'%m\'}',
+      },
+      // 日志文件按日期（天）切割
+      pattern: 'yyyyMMdd',
+      daysToKeep: 60,
+      // maxLogSize: 10485760,
+      numBackups: 3,
+      keepFileExt: true,
+    },
+    errorFile: {
+      type: 'dateFile',
+      filename: `${baseLogPath}/errors/error.log`,
+      alwaysIncludePattern: true,
+      layout: {
+        type: 'pattern',
+        pattern:
+          '{"date":"%d","level":"%p","category":"%c","host":"%h","pid":"%z","data":\'%m\'}',
+      },
+      // 日志文件按日期（天）切割
+      pattern: 'yyyyMMdd',
+      daysToKeep: 60,
+      // maxLogSize: 10485760,
+      numBackups: 3,
+      keepFileExt: true,
     },
     errors: {
-      type: '@log4js-node/logstash-http',
-      url: 'http://110.40.192.199:9200/_bulk',
-      application: 'nest-admin',
-      logType: 'errors',
-      logChannel: 'node',
+      type: 'logLevelFilter',
+      level: 'ERROR',
+      appender: 'errorFile',
     },
   },
   categories: {
     default: {
-      appenders: ['app', 'errors', 'logstash'],
+      appenders: ['console', 'app', 'errors'],
       level: 'DEBUG',
     },
-    info: { appenders: ['app', 'errors', 'logstash'], level: 'info' },
-    access: { appenders: ['app', 'errors', 'logstash'], level: 'info' },
-    http: { appenders: ['access', 'logstash'], level: 'DEBUG' },
+    info: { appenders: ['console', 'app', 'errors'], level: 'info' },
+    access: { appenders: ['console', 'app', 'errors'], level: 'info' },
+    http: { appenders: ['access'], level: 'DEBUG' },
   },
   pm2: true, // 使用 pm2 来管理项目时，打开
   pm2InstanceVar: 'INSTANCE_ID', // 会根据 pm2 分配的 id 进行区分，以免各进程在写日志时造成冲突
